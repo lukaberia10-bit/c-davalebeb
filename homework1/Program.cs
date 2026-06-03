9c76a4749b587a03a5978a8950790fdb55dce85d
user <user@DESKTOP-CMRU34T>
2026-06-03 19:39:07 +0400

    Add project files.

---
+++ homework1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter age");
+
+            //string ageString = Console.ReadLine();
+
+            //bool isvalid = int.TryParse(ageString, out int age);
+
+            //if (isvalid && age >= 18)
+            //{
+            //    Console.WriteLine("you can vote ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you cant vote ");
+            //}
+
+
+
+
+            //Console.WriteLine("enter first number");
+            //double a = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("enter second number");
+            //double b = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("enter third number");
+            //double c = double.Parse(Console.ReadLine());
+
+            //double max;
+
+            //if( a >= b && a >= c)
+            //{
+            //    max = a;
+            //}
+            //else if( b >= a &&  b >= c ) 
+            //{
+            //    max = b;
+            //}
+            //else
+            //{
+            //    max = c;
+            //}
+
+            //Console.WriteLine("biggest number " + max);
+
+
+
+
+
+
+
+            //Console.WriteLine("enter first number");
+
+            //int first = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("enter second number");
+
+            //int second = int.Parse(Console.ReadLine());
+
+
+            //int sum = first + second;
+
+            //if (first == second)
+            //{
+            //    sum = sum * 3;
+            //}
+
+            //Console.WriteLine(sum);
+
+
+            //Console.ReadKey();
+        }
+    }
+}
