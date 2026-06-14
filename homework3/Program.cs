14a0a87a11c555d10be97e20d564e1bcf2b874e4
lukauser <lukaberia10@gmail.com>
2026-06-15 00:23:53 +0400

    dwfsdf

---
+++ homework3/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Number");
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++) {
+
+            //    Console.WriteLine($"{number} * {i} = {number * i}");
+
+
+            //}   
+
+            //Console.WriteLine("Enter  Size: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i++)
+            //{
+
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(" *");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+
+            //Console.Write("Enter Number: ");
+            //int limiti = int.Parse(Console.ReadLine());
+            //int sum = 0;
+
+
+            //for (int i = 2; i <= limiti; i += 2)
+            //{
+            //    sum += i;
+            //}
+
+            //Console.WriteLine($"total: {sum}");
+
+
+            //Random random = new Random();
+
+
+            //int randomNumber = random.Next(1, 101);
+            //int guess = 0;
+
+            //Console.WriteLine("0 to 100 random num chosen by randomiser");
+
+
+            //while (guess != randomNumber)
+            //{
+            //    Console.Write("Enter your guess: ");
+            //    guess = int.Parse(Console.ReadLine());
+
+            //    if (guess < randomNumber)
+            //    {
+            //        Console.WriteLine("your nuber is smaller ");
+            //    }
+            //    else if (guess > randomNumber)
+            //    {
+            //        Console.WriteLine("your nuber is bigger ");
+
+            //    }
+            //}
+
+            //Console.WriteLine("Correct");
+            //Console.WriteLine(randomNumber);
+
+        }
+    }
+}
---
+++ homework4/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array1 = { 1, 2, 3 };
+            //int[] array2 = { 4, 5, 6 };
+
+            //int[] resultArray = new int[array1.Length + array2.Length];
+
+            //Array.Copy(array1, 0, resultArray, 0, array1.Length);
+            //Array.Copy(array2, 0, resultArray, array1.Length, array2.Length);
+
+            //Console.Write("ResultArray = [");
+            //for (int i = 0; i < resultArray.Length; i++)
+            //{
+            //    Console.Write(resultArray[i]);
+            //    if (i < resultArray.Length - 1) Console.Write(", ");
+            //}
+            //Console.WriteLine("]");
+
+
+            //int[] array = { 3, 5, -4, 8, 11, 1, -1, 6 };
+
+            //Console.Write("Enter Target TargetSum: ");
+            //int.TryParse(Console.ReadLine(), out int targetSum);
+
+            //List<int[]> resultList = new List<int[]>();
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = i + 1; j < array.Length; j++)
+            //    {
+            //        if (array[i] + array[j] == targetSum)
+            //        {
+            //            resultList.Add(new int[] { array[i], array[j] });
+            //        }
+            //    }
+            //}
+
+            //Console.Write("resultArray = [");
+            //for (int i = 0; i < resultList.Count; i++)
+            //{
+            //    Console.Write($"[{resultList[i][0]}, {resultList[i][1]}]");
+            //    if (i < resultList.Count - 1) Console.Write(", ");
+            //}
+            //Console.WriteLine("]");
+        }
+    }
+}
