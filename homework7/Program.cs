02d0b68f80236cb6a6d96dd8bcf55b126aeaedc1
lukauser <lukaberia10@gmail.com>
2026-06-29 00:27:27 +0400

    sdfsfsd

---
+++ homework7/Program.cs
@@ -0,0 +1,24 @@
+﻿using static homework7.product;
+
+namespace homework7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Pizza",
+                Price = 25,
+                Nuts = false,
+                Image = "pizza.jpg",
+                Vegeterian = false,
+                Spiciness = 7,
+                Category = "Fast Food"
+            };
+
+            product.PrintInfo();
+        }
+    }
+}
---
+++ homework7/product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework7
+{
+    internal class product
+    {
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public bool Nuts { get; set; }
+            public string Image { get; set; }
+            public bool Vegeterian { get; set; }
+            public int Spiciness { get; set; }
+            public string Category { get; set; }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"ID: {Id}");
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Price: {Price}");
+                Console.WriteLine($"Category: {Category}");
+            }
+        }
+    }
+}
